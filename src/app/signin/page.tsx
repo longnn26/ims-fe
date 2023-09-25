2197c4b9c704a9ebb30dc398b832b520d8a7233f
Trần Anh Tuấn <tuananhtran1410@gmail.com>
2023-09-26 00:11:29 +0700

    Login Header

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LoginComponent from "@/components/Login";
+
+const Signin = () => {
+  return (
+    <>
+      <LoginComponent />
+    </>
+  );
+};
+
+export default Signin;
---
+++ src/components/Login.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, Row, Col } from "antd";
+import styles from "../styles/login.module.css";
+import { Card } from "antd/lib";
+const LoginComponent: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Row justify="center" align="middle" style={{ minHeight: "90vh" }}>
+      <Col span={12}>
+        <Card className={styles["login-card"]}>
+          <h1 style={{ textAlign: "center" }}>LOGIN</h1>
+          <Form
+            name="normal_login"
+            className={styles["login-form"]}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a
+                className="login-form-forgot"
+                style={{ float: "right" }}
+                href=""
+              >
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                style={{ width: "100%" }}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginComponent;
