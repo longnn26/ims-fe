a45abb4bfa110cf621b963f27c03d1489514e134
lieudvq-hisoft <lieudvq@hisoft.vn>
2023-11-13 13:25:38 +0700

    Feat

--- slices/index.ts
+++ slices/index.ts
@@ -1,11 +1,5 @@
 import global from "@slices/global";
-import language from "@slices/language";
-import blog from "@slices/blog";
-import category from "@slices/category";
 
 export default {
   global,
-  language,
-  blog,
-  category,
 };
