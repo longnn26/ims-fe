82a7c555747fe084d22f89c68ce5dac299554dd5
JSum-PhamNhatHa <jsum.webdev@gmail.com>
2023-12-27 21:50:26 +0700

    Update ModalCreate.tsx

--- components/appointment/ModalCreate.tsx
+++ components/appointment/ModalCreate.tsx
@@ -372,7 +372,7 @@ const ModalCreate: React.FC<Props> = (props) => {
                     }}
                   >
                     {selectedReason === "Install" ? requestExpand
-                      .filter((l) => (l.requestType === "Expand" && (l.status === "Working" || l.status === "Accepted")))
+                      .filter((l) => (l.requestType === "Expand" && (l.status === "Waiting" || l.status === "Accepted")))
                       .map((l, index) => (
                         <Option value={l.id} key={index}>
                           {`${l?.id} - Installation`}
