1500b5301f2a3b613e7ab4fc3135f860806a1a25
Trần Anh Tuấn <tuananhtran1410@gmail.com>
2024-01-10 10:56:10 +0700

    Update

--- src/pages/server/[serverAllocationId]/incident/[incidentId].tsx
+++ src/pages/server/[serverAllocationId]/incident/[incidentId].tsx
@@ -200,7 +200,7 @@ const IncidentDetail: React.FC = () => {
             ) : (
               <div className="flex flex-wrap items-center justify-between mb-4 p-2 bg-[#f8f9fa]/10 border border-gray-200 rounded-lg shadow-lg shadow-[#e7edf5]/50">
                 <BreadcrumbComponent itemBreadcrumbs={itemBreadcrumbs} />
-                {Boolean(incidentDetail?.isResolvByClient === true) && (
+                {Boolean(incidentDetail?.isResolvByClient === false) && (
                   <Button
                     type="primary"
                     className="mb-2"
