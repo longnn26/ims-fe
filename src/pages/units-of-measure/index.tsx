8734c8513f868190ced931c08109a81b942d6d18
lieudvq-hisoft <lieudvq@hisoft.vn>
2024-07-13 11:52:19 +0700

    Update index.tsx

--- src/pages/units-of-measure/index.tsx
+++ src/pages/units-of-measure/index.tsx
@@ -5,30 +5,32 @@ import useSelector from "@hooks/use-selector";
 import { getUomCategories, setPageIndex } from "@slices/uomCategory";
 import dynamic from "next/dynamic";
 import React from "react";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import UomCategoryTable from "@components/units-of-measure/UomCategoryTable";
 import { Pagination } from "antd";
+import { GetServerSideProps } from "next";
 
+interface Props {
+  accessToken: string;
+}
 const AntdLayoutNoSSR = dynamic(() => import("@layout/AntdLayout"), {
   ssr: false,
 });
 
-const UnitsOfMeasure: React.FC = () => {
+const UnitsOfMeasure: React.FC<Props> = (props) => {
   const dispatch = useDispatch();
-  const { data: session } = useSession();
+  const { accessToken } = props;
   const { data, pageIndex, pageSize, totalPage } = useSelector(
     (state) => state.uomCategory
   );
 
   //function handle
   const fetchData = useCallback(() => {
-    if (session) {
-      dispatch(
-        getUomCategories({
-          token: session.user.access_token!,
-        })
-      );
-    }
+    dispatch(
+      getUomCategories({
+        token: accessToken,
+      })
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pageIndex]);
 
@@ -56,6 +58,18 @@ const UnitsOfMeasure: React.FC = () => {
       }
     />
   );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const session = await getSession(context);
+  const accessToken = session?.user?.access_token!;
+  return {
+    props: {
+      accessToken: accessToken?.toString(),
+    },
+  };
 };
 
 export default UnitsOfMeasure;
